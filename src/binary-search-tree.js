2992ba524937f9a89aac5fef842807a51e5e45b8
tobiyash <70479970+tobiyash@users.noreply.github.com>
2023-04-17 21:27:36 +0300

    feat: binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,39 +8,112 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor () {
+    this._root = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    if (this._root === null) {
+        this._root = newNode;
+    } else {
+        this.insertNode(this._root, newNode);
+    }
+  }
+  
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+        if (node.left === null) {
+            node.left = newNode;
+        } else {
+            this.insertNode(node.left, newNode);
+        }
+    } else {
+        if (node.right === null) {
+            node.right = newNode;
+        } else {
+            this.insertNode(node.right, newNode);
+        }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data, node = this._root) {
+    if (node === null) {
+      return false;
+    } else if (data < node.data) {
+      return this.has(data, node.left);
+    } else if (data > node.data) {
+      return this.has(data, node.right);
+    } else {
+      return true;
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data, node = this._root) {
+    if (node === null) {
+        return null;
+    } else if (data < node.data) {
+        return this.find(data, node.left);
+    } else if (data > node.data) {
+        return this.find(data, node.right);
+    } else {
+      return node;
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data, node = this._root) {
+    if (node === null) {
+      return null;
+  // если данные, которые нужно удалить, меньше, чем данные корня, переходим к левому поддереву
+    } else if (data < node.data) {
+      node.left = this.remove(data, node.left);
+      return node;
+  // если данные, которые нужно удалить, больше, чем данные корня, переходим к правому поддереву
+    } else if (data > node.data) {
+      node.right = this.remove(data, node.right);
+      return node;
+  // если данные такие как данные корня, удаляем узел
+    } else {
+      // удаляем узел без потомков (листовой узел (leaf) или крайний)
+        if (node.left === null && node.right === null) {
+          node = null;
+          return node;
+        }
+      // удаляем узел с одним потомком
+        if (node.left === null) {
+          node = node.right;
+          return node;
+        } else if(node.right === null) {
+          node = node.left;
+          return node;
+        }
+      // удаляем узел с двумя потомками
+      // minNode правого поддерева хранится в новом узле
+        // let newNode = node.right != null ? this.min(node.right) : this.max(node.left);
+        let newNode = this.min(node.right);
+        console.log(newNode);
+        node.data = newNode;
+        node.right = this.remove(newNode, node.right);
+        return node;
+  }
+}
+
+  min(node = this._root) {
+    if (node.left === null)
+      return node.data;
+    else
+      return this.min(node.left);
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  max(node = this._root) {
+    if (node.right === null)
+      return node.data;
+    else
+      return this.max(node.right);
   }
 }
 
